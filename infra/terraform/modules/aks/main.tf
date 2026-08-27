0ea8dbf68b539138626ab326bfc0f4ac4cc78877
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:35:32 +0200

    fix: align Terraform AKS with current Azure identity model

--- infra/terraform/modules/aks/main.tf
+++ infra/terraform/modules/aks/main.tf
@@ -1,45 +1,54 @@
-variable "location" { type = string }
 variable "resource_group_name" { type = string }
-variable "name_prefix" { type = string }
+variable "location" { type = string }
+variable "environment" { type = string }
 variable "subnet_id" { type = string }
+variable "kubernetes_version" { type = string nullable = true }
 variable "node_count" { type = number }
 variable "node_vm_size" { type = string }
-variable "log_analytics_workspace_id" { type = string }
 
-resource "azurerm_kubernetes_cluster" "main" {
-  name = "${var.name_prefix}-aks"
-  location = var.location
+resource "azurerm_log_analytics_workspace" "logs" {
+  name                = "law-aks-${var.environment}"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  dns_prefix = "${var.name_prefix}-aks"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                              = "aks-showcase-${var.environment}"
+  location                          = var.location
+  resource_group_name               = var.resource_group_name
+  dns_prefix                        = "aks-showcase-${var.environment}"
+  kubernetes_version                = var.kubernetes_version
+  oidc_issuer_enabled               = true
+  workload_identity_enabled         = true
+  role_based_access_control_enabled = true
 
   default_node_pool {
-    name = "system"
-    node_count = var.node_count
-    vm_size = var.node_vm_size
-    vnet_subnet_id = var.subnet_id
+    name                = "system"
+    node_count          = var.node_count
+    vm_size             = var.node_vm_size
+    vnet_subnet_id      = var.subnet_id
+    type                = "VirtualMachineScaleSets"
+    os_disk_size_gb     = 50
   }
 
   identity { type = "SystemAssigned" }
 
-  oidc_issuer_enabled = true
-  workload_identity_enabled = true
-  role_based_access_control_enabled = true
-
   network_profile {
-    network_plugin = "azure"
-    network_policy = "azure"
+    network_plugin    = "azure"
+    network_policy    = "azure"
     load_balancer_sku = "standard"
-    outbound_type = "loadBalancer"
+    outbound_type     = "loadBalancer"
   }
 
-  oms_agent {
-    log_analytics_workspace_id = var.log_analytics_workspace_id
-  }
+  oms_agent { log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id }
 
-  tags = { project = "azure-kubernetes-showcase" }
+  tags = { project = "azure-kubernetes-showcase", environment = var.environment }
 }
 
-output "aks_id" { value = azurerm_kubernetes_cluster.main.id }
-output "aks_name" { value = azurerm_kubernetes_cluster.main.name }
-output "aks_identity_principal_id" { value = azurerm_kubernetes_cluster.main.identity[0].principal_id }
-output "oidc_issuer_url" { value = azurerm_kubernetes_cluster.main.oidc_issuer_url }
+output "aks_cluster_id" { value = azurerm_kubernetes_cluster.aks.id }
+output "aks_name" { value = azurerm_kubernetes_cluster.aks.name }
+output "aks_principal_id" { value = azurerm_kubernetes_cluster.aks.identity[0].principal_id }
+output "kubelet_identity_object_id" { value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id }
+output "oidc_issuer_url" { value = azurerm_kubernetes_cluster.aks.oidc_issuer_url }
