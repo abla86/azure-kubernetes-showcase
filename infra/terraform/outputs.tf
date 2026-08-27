fc589a8f96a39064bdd0ca7a1b67e3b087fdc5b4
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:50:16 +0200

    style: format Terraform module

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,7 +1,27 @@
-output "resource_group_name" { value = azurerm_resource_group.rg.name }
-output "vnet_id" { value = module.networking.vnet_id }
-output "aks_name" { value = module.aks.aks_name }
-output "aks_cluster_id" { value = module.aks.aks_cluster_id }
-output "acr_name" { value = module.acr.acr_name }
-output "oidc_issuer_url" { value = module.aks.oidc_issuer_url }
-output "workload_identity_client_id" { value = module.iam.workload_identity_client_id }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_id" {
+  value = module.networking.vnet_id
+}
+
+output "aks_name" {
+  value = module.aks.aks_name
+}
+
+output "aks_cluster_id" {
+  value = module.aks.aks_cluster_id
+}
+
+output "acr_name" {
+  value = module.acr.acr_name
+}
+
+output "oidc_issuer_url" {
+  value = module.aks.oidc_issuer_url
+}
+
+output "workload_identity_client_id" {
+  value = module.iam.workload_identity_client_id
+}
