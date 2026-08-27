68585cf830554bd66d4fd230737e14df0f252f74
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:55:07 +0200

    fix: correct Terraform AKS syntax

--- infra/terraform/modules/aks/main.tf
+++ infra/terraform/modules/aks/main.tf
@@ -1,55 +1,100 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "environment" { type = string }
-variable "subnet_id" { type = string }
-variable "kubernetes_version" { type = string nullable = true }
-variable "node_count" { type = number }
-variable "node_vm_size" { type = string }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "kubernetes_version" {
+  type     = string
+  nullable = true
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "node_vm_size" {
+  type = string
+}
 
 resource "azurerm_log_analytics_workspace" "logs" {
-  name = "law-aks-\${var.environment}"
-  location = var.location
+  name                = "law-aks-${var.environment}"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  sku = "PerGB2018"
-  retention_in_days = 30
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name = "aks-showcase-\${var.environment}"
-  location = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix = "aks-showcase-\${var.environment}"
-  kubernetes_version = var.kubernetes_version
-  oidc_issuer_enabled = true
-  workload_identity_enabled = true
+  name                              = "aks-showcase-${var.environment}"
+  location                          = var.location
+  resource_group_name               = var.resource_group_name
+  dns_prefix                        = "aks-showcase-${var.environment}"
+  kubernetes_version                = var.kubernetes_version
+  oidc_issuer_enabled               = true
+  workload_identity_enabled         = true
   role_based_access_control_enabled = true
 
   default_node_pool {
-    name = "system"
-    node_count = var.node_count
-    vm_size = var.node_vm_size
-    vnet_subnet_id = var.subnet_id
-    type = "VirtualMachineScaleSets"
+    name            = "system"
+    node_count      = var.node_count
+    vm_size         = var.node_vm_size
+    vnet_subnet_id  = var.subnet_id
+    type            = "VirtualMachineScaleSets"
     os_disk_size_gb = 50
   }
 
-  identity { type = "SystemAssigned" }
-
-  network_profile {
-    network_plugin = "azure"
-    network_policy = "azure"
-    load_balancer_sku = "standard"
-    outbound_type = "loadBalancer"
+  identity {
+    type = "SystemAssigned"
   }
 
-  oms_agent { log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id }
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
+    outbound_type     = "loadBalancer"
+  }
 
-  tags = { project = "azure-kubernetes-showcase", environment = var.environment }
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
+  }
+
+  tags = {
+    project     = "azure-kubernetes-showcase"
+    environment = var.environment
+  }
 }
 
-output "aks_cluster_id" { value = azurerm_kubernetes_cluster.aks.id }
-output "aks_name" { value = azurerm_kubernetes_cluster.aks.name }
-output "aks_principal_id" { value = azurerm_kubernetes_cluster.aks.identity[0].principal_id }
-output "kubelet_identity_object_id" { value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id }
-output "oidc_issuer_url" { value = azurerm_kubernetes_cluster.aks.oidc_issuer_url }
-output "log_analytics_workspace_id" { value = azurerm_log_analytics_workspace.logs.id }
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_principal_id" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
+
+output "kubelet_identity_object_id" {
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+}
+
+output "log_analytics_workspace_id" {
+  value = azurerm_log_analytics_workspace.logs.id
+}
