16ff1a8be6b8a5212105d5b4f91fba19317bcec6
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:55:47 +0200

    fix: expose AKS subnet output

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,6 @@
+output "subnet_id" {
+  value = module.networking.subnet_id
+}
 output "resource_group_name" { value = azurerm_resource_group.rg.name }
 output "vnet_id" { value = module.networking.vnet_id }
 output "aks_name" { value = module.aks.aks_name }
