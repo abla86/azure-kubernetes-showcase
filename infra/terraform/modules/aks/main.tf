de1a912071f649e9af2b323f22e869ed27a67cfc
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:50:07 +0200

    style: format AKS Terraform module

--- infra/terraform/modules/aks/main.tf
+++ infra/terraform/modules/aks/main.tf
@@ -1,10 +1,31 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "environment" { type = string }
-variable "subnet_id" { type = string }
-variable "kubernetes_version" { type = string nullable = true }
-variable "node_count" { type = number }
-variable "node_vm_size" { type = string }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "kubernetes_version" {
+  type     = string
+  nullable = true
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "node_vm_size" {
+  type = string
+}
 
 resource "azurerm_log_analytics_workspace" "logs" {
   name                = "law-aks-${var.environment}"
@@ -25,15 +46,17 @@ resource "azurerm_kubernetes_cluster" "aks" {
   role_based_access_control_enabled = true
 
   default_node_pool {
-    name                = "system"
-    node_count          = var.node_count
-    vm_size             = var.node_vm_size
-    vnet_subnet_id      = var.subnet_id
-    type                = "VirtualMachineScaleSets"
-    os_disk_size_gb     = 50
+    name            = "system"
+    node_count      = var.node_count
+    vm_size         = var.node_vm_size
+    vnet_subnet_id  = var.subnet_id
+    type            = "VirtualMachineScaleSets"
+    os_disk_size_gb = 50
   }
 
-  identity { type = "SystemAssigned" }
+  identity {
+    type = "SystemAssigned"
+  }
 
   network_profile {
     network_plugin    = "azure"
@@ -42,13 +65,32 @@ resource "azurerm_kubernetes_cluster" "aks" {
     outbound_type     = "loadBalancer"
   }
 
-  oms_agent { log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id }
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
+  }
 
-  tags = { project = "azure-kubernetes-showcase", environment = var.environment }
+  tags = {
+    project     = "azure-kubernetes-showcase"
+    environment = var.environment
+  }
 }
 
-output "aks_cluster_id" { value = azurerm_kubernetes_cluster.aks.id }
-output "aks_name" { value = azurerm_kubernetes_cluster.aks.name }
-output "aks_principal_id" { value = azurerm_kubernetes_cluster.aks.identity[0].principal_id }
-output "kubelet_identity_object_id" { value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id }
-output "oidc_issuer_url" { value = azurerm_kubernetes_cluster.aks.oidc_issuer_url }
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_principal_id" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
+
+output "kubelet_identity_object_id" {
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+}
