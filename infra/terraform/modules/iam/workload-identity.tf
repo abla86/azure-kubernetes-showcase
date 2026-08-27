d29685c9ef138dbfafa8555f18f7bb2de3054967
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:39:28 +0200

    feat: add federated workload identity

---
+++ infra/terraform/modules/iam/workload-identity.tf
@@ -0,0 +1,30 @@
+variable "workload_identity_name" {
+  description = "Name of the user-assigned managed identity for the API workload."
+  type        = string
+  default     = "id-aks-workload-api"
+}
+
+variable "workload_identity_subject" {
+  description = "Kubernetes service account subject allowed to federate to the managed identity."
+  type        = string
+  default     = "system:serviceaccount:showcase:api-workload-identity-sa"
+}
+
+resource "azurerm_user_assigned_identity" "workload" {
+  name                = var.workload_identity_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_federated_identity_credential" "api" {
+  name                = "fic-api-workload"
+  resource_group_name = var.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.workload.id
+  issuer              = var.oidc_issuer_url
+  audience            = ["api://AzureADTokenExchange"]
+  subject             = var.workload_identity_subject
+}
+
+output "workload_identity_client_id" {
+  value = azurerm_user_assigned_identity.workload.client_id
+}
