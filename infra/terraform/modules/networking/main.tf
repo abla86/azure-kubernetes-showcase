f7e8c8447416fce0d65e0e8dd311c85f12ffd8ae
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:50:14 +0200

    style: format Terraform module

--- infra/terraform/modules/networking/main.tf
+++ infra/terraform/modules/networking/main.tf
@@ -1,6 +1,14 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "environment" { type = string }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-aks-${var.environment}"
@@ -16,5 +24,10 @@ resource "azurerm_subnet" "aks_subnet" {
   address_prefixes     = ["10.200.1.0/24"]
 }
 
-output "subnet_id" { value = azurerm_subnet.aks_subnet.id }
-output "vnet_id" { value = azurerm_virtual_network.vnet.id }
+output "subnet_id" {
+  value = azurerm_subnet.aks_subnet.id
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
