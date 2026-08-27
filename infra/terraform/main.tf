2afa67440e5ab2aca387dc8184cde83e526d68bc
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:32:22 +0200

    feat: complete Terraform Azure architecture

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,37 +1,52 @@
-terraform {
-  required_version = ">= 1.6.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
+module "networking" {
+  source = "./modules/networking"
+  location = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  name_prefix = var.name_prefix
+  vnet_address_space = var.vnet_address_space
+  aks_subnet_prefix = var.aks_subnet_prefix
+  workload_subnet_prefix = var.workload_subnet_prefix
 }
 
-provider "azurerm" {
-  features {}
+module "acr" {
+  source = "./modules/acr"
+  location = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  name_prefix = var.name_prefix
 }
 
-variable "location" {
-  type    = string
-  default = "westeurope"
+module "aks" {
+  source = "./modules/aks"
+  location = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  name_prefix = var.name_prefix
+  subnet_id = module.networking.aks_subnet_id
+  node_count = var.node_count
+  node_vm_size = var.node_vm_size
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
 }
 
-variable "resource_group_name" {
-  type    = string
-  default = "rg-azure-kubernetes-showcase"
+module "iam" {
+  source = "./modules/iam"
+  resource_group_name = azurerm_resource_group.main.name
+  aks_identity_principal_id = module.aks.aks_identity_principal_id
+  acr_id = module.acr.acr_id
+  aks_oidc_issuer_url = module.aks.oidc_issuer_url
+  workload_identity_name = "${var.name_prefix}-workload"
+  workload_service_account = "showcase-api"
+  workload_namespace = "showcase"
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
+  name = var.resource_group_name
   location = var.location
+  tags = { project = "azure-kubernetes-showcase" environment = "demo" }
 }
 
 resource "azurerm_log_analytics_workspace" "main" {
-  name                = "law-azure-kubernetes-showcase"
-  location            = azurerm_resource_group.main.location
+  name = "${var.name_prefix}-logs"
+  location = var.location
   resource_group_name = azurerm_resource_group.main.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+  sku = "PerGB2018"
+  retention_in_days = 30
 }
