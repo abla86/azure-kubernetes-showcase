6252c47882066b7a32e9af8a66e06b149e14ea5b
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:35:18 +0200

    fix: align Terraform AKS with current Azure identity model

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,25 +1,6 @@
-variable "location" {
-  description = "Azure region for all resources."
-  type = string
-  default = "westeurope"
-}
-variable "resource_group_name" {
-  description = "Resource group for the showcase."
-  type = string
-  default = "rg-azure-kubernetes-showcase"
-}
-variable "name_prefix" {
-  description = "Lowercase resource naming prefix."
-  type = string
-  default = "azkshowcase"
-}
-variable "node_count" { type = number default = 2 }
-variable "node_vm_size" { type = string default = "Standard_B2s" }
-variable "vnet_address_space" { type = list(string) default = ["10.20.0.0/16"] }
-variable "aks_subnet_prefix" { type = string default = "10.20.0.0/22" }
-variable "workload_subnet_prefix" { type = string default = "10.20.4.0/22" }
-variable "sites" {
-  description = "Synthetic logical sites used by the multi-site model."
-  type = set(string)
-  default = ["house-01", "house-02", "house-03"]
-}
+variable "resource_group_name" { description = "Azure resource group." type = string default = "rg-azure-kubernetes-showcase" }
+variable "location" { description = "Azure region." type = string default = "norwayeast" }
+variable "environment" { description = "Deployment environment." type = string default = "dev" }
+variable "kubernetes_version" { description = "AKS Kubernetes version. Set null to use the supported default." type = string default = null nullable = true }
+variable "node_count" { description = "Initial AKS system node count." type = number default = 2 }
+variable "node_vm_size" { description = "AKS system node VM size." type = string default = "Standard_D2s_v5" }
