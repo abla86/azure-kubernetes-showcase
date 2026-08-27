a86487d059817e24cf343c54c24f75e9b51147de
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-08-27 05:39:55 +0200

    feat: expose workload identity client id

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -4,3 +4,4 @@ output "aks_name" { value = module.aks.aks_name }
 output "aks_cluster_id" { value = module.aks.aks_cluster_id }
 output "acr_name" { value = module.acr.acr_name }
 output "oidc_issuer_url" { value = module.aks.oidc_issuer_url }
+output "workload_identity_client_id" { value = module.iam.workload_identity_client_id }
