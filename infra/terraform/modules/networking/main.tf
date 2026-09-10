bf5841b5979c7c9eabcf18d1e4f70684bd1507ae
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-11 01:27:13 +0200

    security: associate NSG with AKS subnet

--- infra/terraform/modules/networking/main.tf
+++ infra/terraform/modules/networking/main.tf
@@ -17,11 +17,34 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.200.0.0/16"]
 }
 
+resource "azurerm_network_security_group" "aks" {
+  name                = "nsg-aks-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "allow-azure-load-balancer"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "AzureLoadBalancer"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "subnet-aks"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.200.1.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.aks.id
 }
 
 output "subnet_id" {
