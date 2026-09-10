5deaaaed8c6ef1afe5b945900b66485bf3b99fb0
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-11 01:15:22 +0200

    fix: apply Terraform canonical formatting

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location                           = var.location
+  location = var.location
   tags = {
     project     = "azure-kubernetes-showcase"
     environment = var.environment
@@ -8,17 +8,17 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "networking" {
-  source                             = "./modules/networking"
-  resource_group_name                = azurerm_resource_group.rg.name
-  location                           = var.location
-  environment                        = var.environment
+  source              = "./modules/networking"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  environment         = var.environment
 }
 
 module "acr" {
-  source                             = "./modules/acr"
-  resource_group_name                = azurerm_resource_group.rg.name
-  location                           = var.location
-  environment                        = var.environment
+  source              = "./modules/acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  environment         = var.environment
 }
 
 module "aks" {
@@ -26,16 +26,16 @@ module "aks" {
   resource_group_name                = azurerm_resource_group.rg.name
   location                           = var.location
   environment                        = var.environment
-  subnet_id            = module.networking.subnet_id
-  kubernetes_version   = var.kubernetes_version
-  node_count           = var.node_count
-  node_vm_size         = var.node_vm_size
+  subnet_id                          = module.networking.subnet_id
+  kubernetes_version                 = var.kubernetes_version
+  node_count                         = var.node_count
+  node_vm_size                       = var.node_vm_size
 }
 
 resource "azurerm_application_insights" "app" {
   name                = "appi-showcase-${var.environment}"
-  location                           = var.location
-  resource_group_name                = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
   workspace_id        = module.aks.log_analytics_workspace_id
   tags = {
