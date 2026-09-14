3ce2fcb49c3401c8a6b5aa571254e48626fef2a3
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-14 03:39:37 +0200

    fix: remove duplicate AKS provisioning profile

--- infra/terraform/modules/aks/main.tf
+++ infra/terraform/modules/aks/main.tf
@@ -57,10 +57,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     mode = "Manual"
   }
 
-  node_provisioning_profile {
-    mode = "Manual"
-  }
-
   default_node_pool {
     name            = "system"
     node_count      = var.node_count
